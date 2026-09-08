1469eb71f537027ad2613f3e152f00adf99f1a24
mymiz <Dimanrytir@mail.ru>
2026-09-08 16:28:25 +0400

    commit 2

--- work1.ts
+++ work1.ts
@@ -77,3 +77,60 @@ function canEnter(age:number, hasTicket:boolean):string{
 console.log(canEnter(18,false));
 console.log(canEnter(18,true));
 console.log(canEnter(16,true));
+
+for(let i=2; i <= 10; i = i+2) {
+        console.log(i);
+}
+
+for(let n=10; n >= 2; n = n-2){
+        console.log(n);
+}
+
+for(let h = 1; h <=10; h++){
+    if(h >= 5){
+        console.log(h);
+    }
+}
+
+
+for(let w = 1; w <= 20; w++){
+    if(w % 2===0){
+        console.log(w);
+    }
+}
+
+for(let a = 1; a <= 20; a++){
+    if(a % 2 !== 0 && a > 10){
+        console.log(a);
+    }
+}
+
+for(let q = 20; q >= 1; q--){
+    if(q % 3 === 0){
+        console.log(q);
+    }
+}
+
+for(let e = 1; e <= 30; e++){
+    if(e % 2 === 0 && e > 10 && e < 25){
+        console.log(e)
+    }
+}
+
+for(let a1= 1; a1 <= 15; a1++){
+    if(a1 % 3 === 0){
+        console.log(a1);
+    }
+}
+
+for(let a2 = 20; a2 >= 5; a2--){
+    if(a2 % 2 !== 0){
+        console.log(a2)
+    }
+}
+
+for(let a3 = 1; a3 <= 30; a3++){
+    if(a3>10 && a3 % 3 === 0 && a3<25){
+        console.log(a3);
+    }
+}
