040a9bb0f07161bd292e5ab2885f239c3a758ff9
mymiz <Dimanrytir@mail.ru>
2026-09-10 22:22:36 +0400

    5

---
+++ slice.ts
@@ -0,0 +1,19 @@
+    const numbers1: number [] = [5, 10, 15, 20, 25, 30];
+
+    console.log(numbers1.slice(1, 4));
+
+
+
+    const numbers2: number [] = [10, 20, 30, 40, 50];
+
+    const result = numbers2.slice(0,3)
+
+    console.log(result);
+    console.log(numbers2)
+
+
+
+
+    const numbers: number [] = [5, 10, 15, 20, 25, 30, 35];
+
+    console.log(numbers.slice(3))
