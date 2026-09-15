b52ffe2bc96726cb94ce3f18e4feb6c614fb8264
mymiz <Dimanrytir@mail.ru>
2026-09-16 00:34:08 +0400

    7

---
+++ arrow.ts
@@ -0,0 +1,16 @@
+const sum = (a: number, b: number): number => {
+    return a * b};
+
+console.log(sum(3,5));
+
+
+
+const isAdult = (age:number): boolean => {
+    return age >= 18;
+}
+
+console.log(isAdult(17));
+console.log(isAdult(18));
+
+
+
---
+++ map.ts
@@ -0,0 +1,17 @@
+const numbers1: number [] = [1,2,3,4,5];
+
+const result1 = numbers1.map((number: number) => {
+    return number * 2;
+});
+
+console.log(result1);
+
+
+
+const numbers: number [] = [5, 10, 15, 20];
+
+const result = numbers.map((number:number) => {
+    return number - 3;
+})
+
+console.log(result);
