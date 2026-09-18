74d583fe9a6aa2ad1a9a8a4ef095a5eacd531c9a
mymiz <Dimanrytir@mail.ru>
2026-09-19 03:15:34 +0400

    8

--- find.ts
+++ find.ts
@@ -1,4 +1,4 @@
-const numbers1: number1 [] = [3, 7, 12, 15, 20, 25];
+const numbers1: number [] = [3, 7, 12, 15, 20, 25];
 
 const result1 = numbers1.find((number:number) => {
     return number > 10;
@@ -8,10 +8,20 @@ console.log(result1);
 
 
 
-const numbers: number[] = [4, 8, 11, 14, 17, 20];
+const numbers2: number[] = [4, 8, 11, 14, 17, 20];
 
-const result = numbers.find((number:number) => {
+const result2 = numbers2.find((number:number) => {
     return number % 2 !== 0;
 })
 
-console.log(result);
+console.log(result2);
+
+
+
+const numbers: number [] = [2, 5, 8, 13, 16, 21, 24];
+
+const result = numbers.find((number:number) => {
+    return number > 10 && number % 3 === 0
+})
+
+console.log(result)
